20bd515562288ebb8fe180d6ef17deef66b330c8
github username <robyntrovati@mac.com>
2015-01-10 15:49:48 -0500

    Complete prob 16

---
+++ 16-power-digit-sum.rb
@@ -0,0 +1,8 @@
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26. What is the sum of the digits of the number 2^1000?
+
+
+def power_digit_sum(power)
+  (2**power).to_s.chars.map { |char| char.to_i }.inject(:+)
+end
+
+power_digit_sum(1000)
