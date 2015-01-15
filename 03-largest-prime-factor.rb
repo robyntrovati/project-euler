b4bef8564290aea6b9484f2458e0aff7b8e7f7e9
github username <robyntrovati@mac.com>
2015-01-15 14:52:48 -0500

    Refactor

--- 03-largest-prime-factor.rb
+++ 03-largest-prime-factor.rb
@@ -1,8 +1,8 @@
 # The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143?
 
 
-def prime?(number)
-  (2..(Math.sqrt(number))).each { |d| return false if number % d == 0 }
+def prime?(n)
+  (2..(Math.sqrt(n))).each { |d| return false if n % d == 0 }
   true
 end
 
