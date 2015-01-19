772006665611c0df026c6ec7c0ca47ccd827c476
github username <robyntrovati@mac.com>
2015-01-19 17:37:08 -0500

    Update parameter name

--- 24-lexicographic-permutations.rb
+++ 24-lexicographic-permutations.rb
@@ -1,8 +1,8 @@
 # Project Euler 24 - Lexicographic Permutations (https://projecteuler.net/problem=24)
 
 
-def permutate(nums, nthpermutation)
-  nums.permutation(10).to_a[nthpermutation-1]
+def permutate(nums, nth_permutation)
+  nums.permutation(10).to_a[nth_permutation-1]
 end
 
 permutate([0,1,2,3,4,5,6,7,8,9], 1000000)
