562b9cc4caf8e225b6ffa38ba7c422ced02ee41a
github username <robyntrovati@mac.com>
2015-01-20 15:25:36 -0500

    Refactor

--- 25-1000-digit-fibonacci.rb
+++ 25-1000-digit-fibonacci.rb
@@ -18,14 +18,16 @@
 # What is the first term in the Fibonacci sequence to contain 1000 digits?
 
 
-def sequence(num_digits)
+def sequence(length)
   fib_pair = [1, 2]
   next_num = 0
-  until next_num.to_s.length == num_digits
+  term_count = 3
+  until next_num.to_s.length == length
     next_num = (fib_pair[-1] + fib_pair[-2])
     fib_pair = [fib_pair[-1], next_num]
+    term_count += 1
   end
-  next_num
+  term_count
 end
 
 sequence(1000)
