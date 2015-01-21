2caf50e68ac7b1c0df1a3e076456ce8275e63498
github username <robyntrovati@mac.com>
2015-01-21 14:31:14 -0500

    Fast

--- 12-highly-divisible-triangular-number.rb
+++ 12-highly-divisible-triangular-number.rb
@@ -1,6 +1,28 @@
 # Project Euler 12 - Highly Divisible Triangular Number (https://projecteuler.net/problem=12)
 
 
+require 'mathn'
+ 
+def factors(n)
+  sum = 1
+  n.prime_division.each { |factor| sum *= (factor[1] + 1) }
+  sum
+end
+
+def triangle_numbers(over_n_divisors)
+  triangle_number = 1
+  factor_count = 1
+  until factors(triangle_number) > over_n_divisors
+    factor_count += 1
+    triangle_number += factor_count
+  end
+  triangle_number
+end
+
+triangle_numbers(500)
+
+
+# Impossibly fucking slow
 def triangle_numbers(over_n_divisors)
   top = 2
   divisor_count = 1
@@ -19,5 +41,3 @@ def triangle_numbers(over_n_divisors)
     top += 1
   end
 end
-
-triangle_numbers(500)
