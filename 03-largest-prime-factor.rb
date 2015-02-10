bcd0055dda337386f9471f2222764b40e65abcd9
github username <robyntrovati@mac.com>
2015-02-10 13:28:51 -0500

    Refactor

--- 03-largest-prime-factor.rb
+++ 03-largest-prime-factor.rb
@@ -1,17 +1,16 @@
 # The prime factors of 13195 are 5, 7, 13 and 29. What is the largest prime factor of the number 600851475143?
 
-
 def largest_prime_factor(n)
-  factors = []
+  factor = 0
   divisor = 2
   until n == 1
     if n % divisor == 0
-      factors << divisor
+      factor = divisor
       n /= divisor
     end
     divisor += 1
   end
-  factors[-1]
+  factor
 end
 
 largest_prime_factor(600851475143)
